8160bd100d66e9d91c72489df4d149cd0b1f07d4
Miguel Angel Gonzales <mitronios@gmail.com>
2024-12-06 10:49:59 -0400

    Ref: Add breakpoints to dictionary and change the evaluation inside a while loop

--- f_romanos.py
+++ f_romanos.py
@@ -1,13 +1,22 @@
-#Iniciaremos creando funciones que nos ayuden a generar números romanos
-#Creamos nuestros números
-numeros_romanos = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
+#Iniciaremos creando funciones que nos ayuden a generar números romanos siguiendo las reglas.
+
+#Creamos nuestros números con valores de "quiebre"
+
+numeros_romanos = { 
+    #Siguiendo la regla de mayor a menor.
+    'M': 1000,
+    'CM': 900,
     'D': 500,
-    'M': 100
+    'CD': 400,
+    'C': 100,
+    'XC': 90,
+    'L': 50,
+    'XL': 40,
+    'X': 10,
+    'IX': 9,
+    'V': 5,
+    'IV': 4,
+    'I': 1
 }
 
 #Ahora creamos un conversor que me toma un número entero y me devuelve romano(str)
@@ -18,12 +27,16 @@ def convierte_a_romanos(valor: int) -> str:
     """
     #Inicio el valor
     romano = ""
+
     #Itero por el diccionario
     for key, value in numeros_romanos.items():
-        #si el valor del dict es = al valor ingresado
-        if value == valor:
-            #El romano será igual a la clave
-            romano = key
+        #Mientras el valor sea mayor o igual a algún valor de value 
+        while valor >= value:
+            #Guardo la clave que corresponde en romano
+            romano = romano + key
+            #Resto el valor restante para conocer el siguiente valor
+            valor -= value # 58 cabe una vez en L, 58 - 50 sobran 8, 5 cabe  en V quedan 3 y esos 3 caben en III
+
     #Devuelvo el romano
     return romano
 
@@ -32,5 +45,5 @@ def convierte_a_romanos(valor: int) -> str:
 assert convierte_a_romanos(1) == 'I', "Conversión incorrecta"
 assert convierte_a_romanos(500) == 'D', "Conversión incorrecta"
 
-assert convierte_a_romanos(2) == 'II'
-assert convierte_a_romanos(200) == 'CC'
+assert convierte_a_romanos(2) == 'II', "conversión incorrecta"
+assert convierte_a_romanos(200) == 'CC', "Conversión incorrecta"
